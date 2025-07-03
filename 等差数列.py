97930f33e168e7266148985bfb9a462c4a8db9ba
kyota <lupiys000@gmail.com>
2025-07-03 16:18:04 +0900

    むずかしい

--- 等差数列.py
+++ 等差数列.py
@@ -3,9 +3,13 @@ H,W = int(PI[0]),int(PI[1])
 nums1 = list(map(int,input().split()))
 nums2 = list(map(int,input().split()))
 diff1,diff2 = nums1[1] - nums1[0],nums2[0] - nums1[0]
-print(H,W,nums1,nums2,diff1,diff2)
-seq = [nums1]
-for i in range(2,H):
-    for j in range(2,W):
-       seq.append(int(int(nums1[H - 1])+diff1))
-       print(seq)
+seq = []
+for i in range(H):
+    val = []
+    
+    for j in range(W):
+        val.append(nums1[0] + diff2 * i + diff1 * j)
+    
+    seq.append(val)
+for i in range(H):
+    print(*seq[i])    
