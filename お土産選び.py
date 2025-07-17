32ae72242306b58106d6d7fe98eeb0b439266acf
kyota <lupiys000@gmail.com>
2025-07-17 23:50:57 +0900

    難しいです、とりあえずadd

---
+++ お土産選び.py
@@ -0,0 +1,16 @@
+N = int,input()
+
+pack = []
+
+for _ in range(3):
+    pack.append(list(map(int,input().split())))
+    
+print(pack)
+min_now = 0
+manju = 0
+max_loop1,max_loop2,max_loop3 = pack[]
+while :
+    
+
+"""while manju <= 20:
+    min = 0"""
