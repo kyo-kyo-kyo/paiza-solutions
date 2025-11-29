3d3a2f34e73bafaf0baf31763f14b24137bd0960
kyota <lupiys000@gmail.com>
2025-11-29 15:48:43 +0900

    調べる場所を黒石から何も置いてない場所に変更。関数で何にも引っかからなかったときに0を返すのを忘れない

--- 黒石.py
+++ 黒石.py
@@ -1,5 +1,5 @@
 def Search(x,y):
-    can_now = 0
+    #can_now = 0
     around = [
         [-1,1],[0,1],[1,1],
         [1,0],[1,-1],[0,-1],
@@ -11,9 +11,11 @@ def Search(x,y):
         if 0 <= nx < N and 0<= ny < N:
             
             if Board[x+i][y+j] == "W":
-                can_now += Search_same(x,y,i,j)
-            
-    return can_now
+                #can_now += Search_same(x,y,i,j)
+                if Search_same(x,y,i,j) == 1:
+                    return 1
+    #return can_now
+    return 0
         
 def Search_same(x,y,i,j):
     a,b = x+i,y+j
@@ -22,7 +24,7 @@ def Search_same(x,y,i,j):
     
     if Board[a][b] == "W":
         return Search_same(x+i,y+j,i,j)
-    elif Board[a][b] == ".":
+    elif Board[a][b] == "B":
         return 1
     else:
         return 0
@@ -34,7 +36,7 @@ can = 0
 
 for y in range(N):
     for x in range(N):
-        if Board[x][y] == "B":
+        if Board[x][y] == ".":
             can += Search(x,y)
             
 print(can)
